2197a148220f635e74bd2d44b65521673ef75201
dmoscoa1009 <dmoscoa1009@gmail.com>
2025-06-13 13:56:23 -0600

    fix: server port correction

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -34,5 +34,5 @@ app.use(
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
