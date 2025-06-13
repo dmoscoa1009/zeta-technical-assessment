2db8559dd385b7c89180d7aecdd20de939e6c5dc
dmoscoa1009 <dmoscoa1009@gmail.com>
2025-06-12 20:48:49 -0600

    feat: added auth and admin middlewares to some product routes

--- apps/server/src/routes/product.routes.ts
+++ apps/server/src/routes/product.routes.ts
@@ -6,13 +6,19 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/product.controller";
+import { adminMiddleware, authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/get-all-products", getProducts);
 router.get("/get-product-by-id/:id", getProductById);
-router.post("/create-product", createProduct);
-router.put("/update-product/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/create-product", authMiddleware, adminMiddleware, createProduct);
+router.put(
+  "/update-product/:id",
+  authMiddleware,
+  adminMiddleware,
+  updateProduct
+);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteProduct);
 
 export default router;
